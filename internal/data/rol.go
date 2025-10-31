1aa070620042f16db91b311405bf7f8be9294516
Loui27 <luis.sanchezv@pucp.edu.pe>
2025-10-31 07:43:19 -0500

    Change 1
    Prueba de creación de files

---
+++ internal/data/rol.go
@@ -0,0 +1,6 @@
+package data
+
+type Rol struct {
+	id     int
+	nombre string
+}
---
+++ internal/data/ticket.go
@@ -0,0 +1,11 @@
+package data
+
+type ticket struct {
+	id          int
+	idOrden     int
+	idEvento    int
+	codigoQR    string
+	tipo        string
+	precioVenta float64
+	estado      string
+}

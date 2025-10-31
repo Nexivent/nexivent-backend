95ada0416b72bda46883b289f5a44b0290588a4e
thought-eater <135585422+thought-eater@users.noreply.github.com>
2025-10-31 10:58:38 -0500

    Add panic recovery

--- internal/middleware/context.go
+++ internal/middleware/context.go
@@ -1,6 +1,7 @@
 package middleware
 
 import (
+	"fmt"
 	"net/http"
 
 	appcontext "github.com/Nexivent/nexivent-backend/internal/context"
@@ -12,6 +13,24 @@ import (
 func InjectApplication(app *settings.Application) func(http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			defer func() {
+				// Use the builtin recover function to check if there has been a panic or
+				// not.
+				if err := recover(); err != nil {
+					// If there was a panic, set a "Connection: close" header on the
+					// response. This acts as a trigger to make Go's HTTP server
+					// automatically close the current connection after a response has been
+					// sent.
+					w.Header().Set("Connection", "close")
+					// The value returned by recover() has the type any, so we use
+					// fmt.Errorf() to normalize it into an error and call our
+					// serverErrorResponse() helper. In turn, this will log the error using
+					// our custom Logger type at the ERROR level and send the client a 500
+					// Internal Server Error response.
+					app.ServerErrorResponse(w, r, fmt.Errorf("%s", err))
+				}
+			}()
+
 			// Añadir la aplicación al contexto del request
 			ctx := appcontext.WithApplication(r.Context(), app)
 			// Crear un nuevo request con el contexto actualizado
--- internal/routing/routing.go
+++ internal/routing/routing.go
@@ -3,14 +3,23 @@ package routing
 import (
 	"net/http"
 
-	"github.com/julienschmidt/httprouter"
 	"github.com/Nexivent/nexivent-backend/internal/middleware"
 	"github.com/Nexivent/nexivent-backend/internal/settings"
+	"github.com/julienschmidt/httprouter"
 )
 
 func Routes(app *settings.Application) http.Handler {
 	router := httprouter.New()
+	
+	// Convert the notFoundResponse() helper to a http.Handler using the
+	// http.HandlerFunc() adapter, and then set it as the custom error handler for 404
+	// Not Found responses.
+	router.NotFound = http.HandlerFunc(app.NotFoundResponse)
 
+	// Likewise, convert the methodNotAllowedResponse() helper to a http.Handler and set
+	// it as the custom error handler for 405 Method Not Allowed responses.
+	router.MethodNotAllowed = http.HandlerFunc(app.MethodNotAllowedResponse)
+	
 	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", healthcheckHandler)
 	router.HandlerFunc(http.MethodGet, "/v1/eventos/:id", getEvent)
 

55751ece928496b868101887843fec4eae970f3e
Loui27 <luis.sanchezv@pucp.edu.pe>
2025-11-02 23:15:26 -0500

    Correcion de SQL query
    creacion de tabla de BD

--- migrations/001_create_tables.sql
+++ migrations/001_create_tables.sql
@@ -4,13 +4,13 @@ CREATE TABLE usuario (
     usuario_id BIGSERIAL PRIMARY KEY,
     nombre VARCHAR(80) NOT NULL,
     tipo_documento tipo_documento_enum NOT NULL,
-    num_documento VARCHAR (25) NOT NULL,
-    correo VARCHAR (80) NOT NULL UNIQUE,
-    contrasenha VARCHAR (255) NOT NULL,
+    num_documento VARCHAR(25) NOT NULL,
+    correo VARCHAR(80) NOT NULL UNIQUE,
+    contrasenha VARCHAR(255) NOT NULL,
     --hash
-    telefono VARCHAR (15),
+    telefono VARCHAR(15),
     estado_de_cuenta SMALLINT NOT NULL DEFAULT 0,
-    codigo_verificacion VARCHAR (10),
+    codigo_verificacion VARCHAR(10),
     fecha_expiracion_codigo TIMESTAMP,
     usuario_creacion BIGINT,
     fecha_creacion TIMESTAMPTZ NOT NULL DEFAULT NOW(),
@@ -47,7 +47,7 @@ CREATE TABLE evento (
     usuario_modificacion BIGINT,
     fecha_modificacion TIMESTAMPTZ,
     CONSTRAINT fk_evento_organizador FOREIGN KEY (organizador_id) REFERENCES usuario(usuario_id) ON DELETE RESTRICT,
-    CONSTRAINT fk_evento_categoria FOREIGN KEY (categoria_id) REFERENCES categoria(categoria_id) ON DELETE RESTRICT,
+    CONSTRAINT fk_evento_categoria FOREIGN KEY (categoria_id) REFERENCES categoria(id_categoria) ON DELETE RESTRICT,
     CONSTRAINT chk_evento_estado CHECK (evento_estado IN (0, 1, 2)),
     CONSTRAINT chk_evento_activo CHECK (activo IN (0, 1))
 );
@@ -83,7 +83,7 @@ DROP TABLE IF EXISTS tipo_de_ticket;
 CREATE TABLE tipo_de_ticket (
     tipo_de_ticket_id BIGSERIAL PRIMARY KEY,
     evento_id BIGINT NOT NULL,
-    nombre VARCHAR (25) NOT NULL,
+    nombre VARCHAR(25) NOT NULL,
     fecha_ini DATE NOT NULL,
     fecha_fin DATE NOT NULL,
     activo SMALLINT NOT NULL DEFAULT 1,
@@ -91,14 +91,15 @@ CREATE TABLE tipo_de_ticket (
     fecha_creacion TIMESTAMPTZ NOT NULL DEFAULT NOW(),
     usuario_modificacion BIGINT,
     fecha_modificacion TIMESTAMPTZ,
-    CONSTRAINT fk_tipo_de_ticket_evento FOREIGN KEY (evento_id) REFERENCES evento(evento_id) ON DELETE RESTRICT CONSTRAINT uq_tipo_ticket_nombre UNIQUE (evento_id, nombre),
+    CONSTRAINT fk_tipo_de_ticket_evento FOREIGN KEY (evento_id) REFERENCES evento(evento_id) ON DELETE RESTRICT,
+    CONSTRAINT uq_tipo_ticket_nombre UNIQUE (evento_id, nombre),
     CONSTRAINT chk_tipo_de_ticket_activo CHECK (activo IN (0, 1))
 );
 DROP TABLE IF EXISTS perfil_de_persona;
 CREATE TABLE perfil_de_persona (
     perfil_de_persona_id BIGSERIAL PRIMARY KEY,
     evento_id BIGINT NOT NULL,
-    nombre VARCHAR (25) NOT NULL,
+    nombre VARCHAR(25) NOT NULL,
     activo SMALLINT NOT NULL DEFAULT 1,
     usuario_creacion BIGINT,
     fecha_creacion TIMESTAMPTZ NOT NULL DEFAULT NOW(),
@@ -107,7 +108,8 @@ CREATE TABLE perfil_de_persona (
     CONSTRAINT fk_perfil_de_persona_evento FOREIGN KEY (evento_id) REFERENCES evento(evento_id) ON DELETE RESTRICT,
     CONSTRAINT uq_perfil_de_persona_nombre UNIQUE (evento_id, nombre),
     CONSTRAINT chk_perfil_de_persona_activo CHECK (activo IN (0, 1))
-) DROP TABLE IF EXISTS tarifa;
+);
+DROP TABLE IF EXISTS tarifa;
 CREATE TABLE tarifa (
     tarifa_id BIGSERIAL PRIMARY KEY,
     sector_id BIGINT NOT NULL,
@@ -126,14 +128,14 @@ CREATE TABLE tarifa (
 );
 CREATE TYPE tipo_metodo_pago_enum AS ENUM ('Tarjeta', 'Yape');
 DROP TABLE IF EXISTS metodo_de_pago;
-CREATE TABLE IF EXISTS metodo_de_pago(
+CREATE TABLE metodo_de_pago (
     metodo_de_pago_id BIGSERIAL PRIMARY KEY,
     tipo tipo_metodo_pago_enum NOT NULL,
     activo SMALLINT NOT NULL DEFAULT 1,
     CONSTRAINT chk_metodo_de_pago_activo CHECK (activo IN (0, 1))
 );
 DROP TABLE IF EXISTS orden_de_compra;
-CREATE TABLE orden_de_compra(
+CREATE TABLE orden_de_compra (
     orden_de_compra_id BIGSERIAL PRIMARY KEY,
     usuario_id BIGINT NOT NULL,
     metodo_de_pago_id BIGINT NOT NULL,
@@ -146,12 +148,12 @@ CREATE TABLE orden_de_compra(
     CONSTRAINT chk_orden_de_compra_estado CHECK (estado_de_orden IN (0, 1, 2))
 );
 DROP TABLE IF EXISTS fecha;
-CREATE TABLE fecha(
+CREATE TABLE fecha (
     fecha_id BIGSERIAL PRIMARY KEY,
-    fecha_evento DATE NOT NULL,
+    fecha_evento DATE NOT NULL
 );
 DROP TABLE IF EXISTS evento_fecha;
-CREATE TABLE evento_fecha(
+CREATE TABLE evento_fecha (
     evento_fecha_id BIGSERIAL PRIMARY KEY,
     evento_id BIGINT NOT NULL,
     fecha_id BIGINT NOT NULL,
@@ -163,7 +165,7 @@ CREATE TABLE evento_fecha(
     fecha_modificacion TIMESTAMPTZ,
     CONSTRAINT fk_evento_fecha_evento FOREIGN KEY (evento_id) REFERENCES evento(evento_id) ON DELETE RESTRICT,
     CONSTRAINT fk_evento_fecha_fecha FOREIGN KEY (fecha_id) REFERENCES fecha(fecha_id) ON DELETE RESTRICT,
-    CONSTRAINT uq_evento_fecha UNIQUE (evento_id, fecha_id, horaInicio),
+    CONSTRAINT uq_evento_fecha UNIQUE (evento_id, fecha_id, hora_inicio),
     CONSTRAINT chk_evento_fecha_activo CHECK (activo IN (0, 1))
 );
 DROP TABLE IF EXISTS ticket;
@@ -178,8 +180,9 @@ CREATE TABLE ticket (
     CONSTRAINT fk_ticket_fecha FOREIGN KEY (evento_fecha_id) REFERENCES evento_fecha(evento_fecha_id),
     CONSTRAINT fk_ticket_tarifa FOREIGN KEY (tarifa_id) REFERENCES tarifa(tarifa_id),
     CONSTRAINT chk_ticket_estado CHECK (estado_de_ticket IN (0, 1, 2, 3))
-) DROP TABLE IF EXISTS cupon;
-CREATE TABLE cupon(
+);
+DROP TABLE IF EXISTS cupon;
+CREATE TABLE cupon (
     cupon_id BIGSERIAL PRIMARY KEY,
     descripcion TEXT NOT NULL,
     tipo VARCHAR(20) NOT NULL,
@@ -196,7 +199,7 @@ CREATE TABLE cupon(
     CONSTRAINT chk_cupon_estado CHECK (estado_cupon IN (0, 1))
 );
 DROP TABLE IF EXISTS evento_cupon;
-CREATE TABLE evento_cupon(
+CREATE TABLE evento_cupon (
     evento_id BIGINT NOT NULL,
     cupon_id BIGINT NOT NULL,
     cant_cupones BIGINT NOT NULL,
@@ -212,16 +215,16 @@ CREATE TABLE evento_cupon(
     CONSTRAINT chk_evento_cupon_rango CHECK (fecha_fin >= fecha_ini)
 );
 DROP TABLE IF EXISTS usuario_cupon;
-CREATE TABLE usuario_cupon(
+CREATE TABLE usuario_cupon (
     cupon_id BIGINT NOT NULL,
     usuario_id BIGINT NOT NULL,
     cant_usada BIGINT NOT NULL DEFAULT 0,
     CONSTRAINT pk_usuario_cupon PRIMARY KEY (cupon_id, usuario_id),
     CONSTRAINT fk_usuario_cupon_usuario FOREIGN KEY (usuario_id) REFERENCES usuario(usuario_id) ON DELETE RESTRICT,
-    CONSTRAINT fk_evento_cupon_cupon FOREIGN KEY (cupon_id) REFERENCES cupon(cupon_id) ON DELETE RESTRICT,
+    CONSTRAINT fk_usuario_cupon_cupon FOREIGN KEY (cupon_id) REFERENCES cupon(cupon_id) ON DELETE RESTRICT
 );
 DROP TABLE IF EXISTS comprobante_de_pago;
-CREATE TABLE comprobante_de_pago(
+CREATE TABLE comprobante_de_pago (
     comprobante_de_pago_id BIGSERIAL PRIMARY KEY,
     orden_de_compra_id BIGINT NOT NULL,
     tipo_de_comprobante SMALLINT NOT NULL DEFAULT 0,
@@ -233,16 +236,16 @@ CREATE TABLE comprobante_de_pago(
     CONSTRAINT chk_comprobante_de_pago_tipo CHECK (tipo_de_comprobante IN (0, 1))
 );
 DROP TABLE IF EXISTS rol;
-CREATE TABLE rol(
+CREATE TABLE rol (
     rol_id BIGSERIAL PRIMARY KEY,
     nombre VARCHAR(20) NOT NULL,
     usuario_creacion BIGINT,
     fecha_creacion TIMESTAMPTZ NOT NULL DEFAULT NOW(),
     usuario_modificacion BIGINT,
-    fecha_modificacion TIMESTAMPTZ,
+    fecha_modificacion TIMESTAMPTZ
 );
 DROP TABLE IF EXISTS rol_usuario;
-CREATE TABLE rol_usuario(
+CREATE TABLE rol_usuario (
     rol_usuario_id BIGSERIAL PRIMARY KEY,
     rol_id BIGINT NOT NULL,
     usuario_id BIGINT NOT NULL,
@@ -254,13 +257,13 @@ CREATE TABLE rol_usuario(
     CONSTRAINT fk_rol_usuario_usuario FOREIGN KEY (usuario_id) REFERENCES usuario(usuario_id)
 );
 DROP TABLE IF EXISTS notificacion;
-CREATE TABLE notificacion(
+CREATE TABLE notificacion (
     notificacion_id BIGSERIAL PRIMARY KEY,
     mensaje TEXT NOT NULL,
     canal VARCHAR(40) NOT NULL,
     fecha_envio TIMESTAMP NOT NULL,
-    estado_notificación SMALLINT NOT NULL,
-    CONSTRAINT chk_notificacion CHECK (notificacion IN (0, 1, 2))
+    estado_notificacion SMALLINT NOT NULL,
+    CONSTRAINT chk_notificacion CHECK (estado_notificacion IN (0, 1, 2))
 );
 -- Índice para búsquedas case-insensitive por nombre (opcional)
-CREATE UNIQUE INDEX IF NOT EXISTS idx_categorias_nombre_ci ON categorias (LOWER(nombre));
+CREATE UNIQUE INDEX IF NOT EXISTS idx_categoria_nombre_ci ON categoria (LOWER(nombre));

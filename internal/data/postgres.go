c6d3cad0dd7ffdd7c5220d2955b96c6579de8ca8
Loui27 <luis.sanchezv@pucp.edu.pe>
2025-11-03 23:18:33 -0500

    Actualizar BD a la nube
    Script actualizado y nueva BD en la nube

--- internal/data/postgres.go
+++ internal/data/postgres.go
@@ -9,22 +9,43 @@ import (
 )
 
 type DBConfig struct {
-	DNS          string
-	MaxOpenConns int
-	MaxIdleConns int
-	MaxIdleTime  time.Duration
+	DNS             string
+	MaxOpenConns    int
+	MaxIdleConns    int
+	MaxIdleTime     time.Duration // p.ej. 15 * time.Minute
+	MaxConnLifetime time.Duration // p.ej. 2 * time.Hour
+	PingTimeout     time.Duration // p.ej. 5 * time.Second
 }
 
-func OpenDB(dbConf DBConfig) (*sql.DB, error) {
-	db, err := sql.Open("pgx", dbConf.DNS)
+func OpenDB(conf DBConfig) (*sql.DB, error) {
+	db, err := sql.Open("pgx", conf.DNS)
 	if err != nil {
 		return nil, err
 	}
-	db.SetMaxOpenConns(dbConf.MaxOpenConns)
-	db.SetMaxIdleConns(dbConf.MaxIdleConns)
-	db.SetConnMaxIdleTime(dbConf.MaxIdleTime)
 
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	// Defaults sensatos si vinieron en cero
+	if conf.MaxOpenConns == 0 {
+		conf.MaxOpenConns = 20
+	}
+	if conf.MaxIdleConns == 0 {
+		conf.MaxIdleConns = 5
+	}
+	if conf.MaxIdleTime == 0 {
+		conf.MaxIdleTime = 15 * time.Minute
+	}
+	if conf.MaxConnLifetime == 0 {
+		conf.MaxConnLifetime = 2 * time.Hour
+	}
+	if conf.PingTimeout == 0 {
+		conf.PingTimeout = 5 * time.Second
+	}
+
+	db.SetMaxOpenConns(conf.MaxOpenConns)
+	db.SetMaxIdleConns(conf.MaxIdleConns)
+	db.SetConnMaxIdleTime(conf.MaxIdleTime)
+	db.SetConnMaxLifetime(conf.MaxConnLifetime)
+
+	ctx, cancel := context.WithTimeout(context.Background(), conf.PingTimeout)
 	defer cancel()
 	if err := db.PingContext(ctx); err != nil {
 		_ = db.Close()
--- nexivent-local.session.sql
+++
@@ -1,2 +0,0 @@
-SELECT *
-FROM orden_de_compra;

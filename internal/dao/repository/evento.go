97cec1f7676c19f5be740b6a4995b765a2110f6d
Maryo07 <mcervera-07@hotmail.com>
2025-11-08 15:26:14 -0500

    Evento create, rename usuario.go file

--- internal/dao/repository/evento.go
+++ internal/dao/repository/evento.go
@@ -10,3 +10,22 @@ type Evento struct {
 	logger       logging.Logger
 	PostgresqlDB *gorm.DB
 }
+
+func NewEventoController(
+	logger logging.Logger,
+	postgresqlDB *gorm.DB,
+) *Evento {
+	return &Evento{
+		logger:       logger,
+		PostgresqlDB: postgresqlDB,
+	}
+}
+
+func (e *Evento) CrearEvento(Evento *model.Evento) (int64, error) {
+	respuesta := e.PostgresqlDB.Create(Evento)
+	if respuesta.Error != nil{
+		return 0, respuesta.Error
+	}
+
+	return respuesta.ID, nil
+}

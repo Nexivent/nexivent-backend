f3c32cccaee6d51af769e4edbbb8e94e3ba1e82a
thought-eater <135585422+thought-eater@users.noreply.github.com>
2025-11-07 19:12:23 -0500

    Change sql to sqlx

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -7,8 +7,10 @@ import (
 	"os"
 	"time"
 
+	"github.com/jmoiron/sqlx"
 	"github.com/Nexivent/nexivent-backend/internal/routing"
 	"github.com/Nexivent/nexivent-backend/internal/settings"
+	_ "github.com/lib/pq"
 )
 
 func main() {
@@ -17,12 +19,16 @@ func main() {
 	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
 
 	settings.ParseFlagEnv(logger, &cfg)
-	
-	db, err := settings.OpenDB(cfg)
+
+	db, err := sqlx.Connect("posgres", cfg.DB.URL)
 	if err != nil {
 		logger.Error(err.Error())
 		os.Exit(1)
 	}
+
+	db.SetMaxOpenConns(cfg.DB.MaxOpenConns)
+	db.SetMaxIdleConns(cfg.DB.MaxIdleConns)
+	db.SetConnMaxIdleTime(cfg.DB.MaxIdleTime)
 
 	defer db.Close()
 

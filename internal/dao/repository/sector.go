34891612d7487e9d58258bfdfde77633e948c30d
Maryo07 <mcervera-07@hotmail.com>
2025-11-09 14:26:22 -0500

    update \repository

--- internal/dao/repository/sector.go
+++ internal/dao/repository/sector.go
@@ -1,12 +1,42 @@
 package repository
 
 import (
+	"github.com/Loui27/nexivent-backend/internal/dao/model"
 	"github.com/Loui27/nexivent-backend/logging"
 	"gorm.io/gorm"
-	// "github.com/Loui27/nexivent-backend/internal/dao/model"
 )
 
 type Sector struct {
 	logger       logging.Logger
 	PostgresqlDB *gorm.DB
+}
+
+func NewSectorController(
+	logger logging.Logger,
+	postgreesqlDB *gorm.DB,
+) *Sector {
+	return &Sector{
+		logger:       logger,
+		PostgresqlDB: postgreesqlDB,
+	}
+}
+
+func (s *Sector) CrearSector(sector *model.Sector) error {
+	resultado := s.PostgresqlDB.Create(sector)
+
+	if resultado.Error != nil {
+		return resultado.Error
+	}
+
+	return nil
+}
+
+func (s *Sector) ActualizarSector(sector *model.Sector) error {
+	respuesta := s.PostgresqlDB.Save(sector)
+
+	if respuesta.Error != nil {
+		return respuesta.Error
+	}
+
+	return nil
 }
--- internal/dao/repository/tipo_de_ticket.go
+++ internal/dao/repository/tipo_de_ticket.go
@@ -1,12 +1,42 @@
 package repository
 
 import (
+	"github.com/Loui27/nexivent-backend/internal/dao/model"
 	"github.com/Loui27/nexivent-backend/logging"
 	"gorm.io/gorm"
-	// "github.com/Loui27/nexivent-backend/internal/dao/model"
 )
 
 type TipoDeTicket struct {
 	logger       logging.Logger
 	PostgresqlDB *gorm.DB
+}
+
+func NewTipoDeTicketController(
+	logger logging.Logger,
+	postgreesqlDB *gorm.DB,
+) *TipoDeTicket {
+	return &TipoDeTicket{
+		logger:       logger,
+		PostgresqlDB: postgreesqlDB,
+	}
+}
+
+func (t *TipoDeTicket) CrearTipoDeTicket(TipoDeTicket *model.TipoDeTicket) error {
+	resultado := t.PostgresqlDB.Create(TipoDeTicket)
+
+	if resultado.Error != nil {
+		return resultado.Error
+	}
+
+	return nil
+}
+
+func (t *TipoDeTicket) ActualizarTipoDeTicketr(TipoDeTicket *model.TipoDeTicket) error {
+	respuesta := t.PostgresqlDB.Save(TipoDeTicket)
+
+	if respuesta.Error != nil {
+		return respuesta.Error
+	}
+
+	return nil
 }
